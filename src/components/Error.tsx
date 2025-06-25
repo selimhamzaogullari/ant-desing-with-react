87a5a16be474337adb640d7772018f53c213e634
selimhamzaogullari <selim.hamzaogullari@fraud.com>
2025-06-25 20:33:12 +0300

    added button in error component to return home page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,10 +1,28 @@
-import { Typography } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Button, Typography } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const navigate = useNavigate();
   return (
-    <Typography.Title level={3} type="danger" style={{ textAlign: "center", padding: 50 }}>
-      Something went wrong!
-    </Typography.Title>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        marginTop: 50,
+        rowGap: 12,
+      }}
+    >
+      <Typography.Title level={3} type="danger">
+        Something went wrong!
+      </Typography.Title>
+      <Button icon={<ArrowLeftOutlined />} onClick={() => navigate("/")}>
+        Return Home Page
+      </Button>
+    </div>
   );
 };
 
